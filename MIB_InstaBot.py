1c22c38179e2a4a331b0c54de8ceb5ba4152658c
Mirza Iliyaz Baig <mib.iliyaz@gmail.com>
2021-08-30 15:42:05 +0530

    Add files via upload

---
+++ MIB_InstaBot.py
@@ -0,0 +1,9 @@
+from instabot import Bot
+bot = Bot()
+bot.login(username = "mib_instabot_demo", password = "mib00786")
+"""bot.follow("ieeemjcet_cis")
+bot.upload_photo("MIB.jpg", caption = "Its me, your MIB")
+bot.send_message("Hi I am a bot","iam_iliyaz")
+bot.get_user_followers('iam_iliyaz')
+bot.get_user_following("iam_iliyaz")
+bot.unfollow("mib.iliyaz")"""
